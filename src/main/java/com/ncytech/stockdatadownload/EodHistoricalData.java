132f430bf68eed4bbc09820d05b3d6a443a57285
Naveen Yarlagadda <ynchowdary@hotmail.com>
2022-05-05 08:26:12 -0500

    Initial Commit

--- src/main/java/com/ncytech/stockdatadownload/EodHistoricalData.java
+++ src/main/java/com/ncytech/stockdatadownload/EodHistoricalData.java
@@ -1,8 +1,14 @@
 package com.ncytech.stockdatadownload;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
-@Component
+@Service
 public class EodHistoricalData {
-    public EodHistoricalData(WebCl)
+    private final WebClient webClient;
+    public EodHistoricalData(WebClient.Builder webClientBulder) {
+        this.webClient = webClientBulder.baseUrl("https://eodhistoricaldata.com/api/").build();
+    }
+
+
 }
